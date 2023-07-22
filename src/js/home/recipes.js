86ddec66fecb54aec5009153d42b26f76f3be94f
AntonTy35 <antony.tislenko@gmail.com>
2023-07-22 21:14:23 +0300

    link

--- src/js/home/recipes.js
+++ src/js/home/recipes.js
@@ -4,20 +4,37 @@ const recipes = document.querySelector('.recipes-js');
 
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
 
-async function recipesMarkup() {
-  //const markup = await fetch();
-  return await fetch(
-    `${BASE_URL}/recipes?limit=9`
-    // ?category=Beef&page=1&limit=6&time=160&area=Irish&ingredient=640c2dd963a319ea671e3796`
-  )
+async function recipesFetch() {
+  const markup = await fetch(`${BASE_URL}/recipes?limit=9`)
     .then(res => {
       if (!res.ok) {
         throw new Error(res.status);
       }
       return res.json();
     })
-    .then(obj => console.log(obj))
+    .then(obj => {
+      //   console.log(obj);
+      console.dir(obj.results);
+      recipes.innerHTML = recipesMarkup(obj.results);
+    })
     .catch(err => console.log(err));
+
+  //   return recipes.insertAdjacentHTML('beforeend', markup);
 }
 
-recipesMarkup();
+// recipesFetch();
+
+function recipesMarkup(arr) {
+  return arr
+    .map(
+      ({ title, thumb }) => `<li>
+        <img src="${thumb}" alt="${title}" width="150">
+        <button type="button">heart</button>
+        <h2>${title}</h2>
+        <p>description</p>
+        <p>rating</p>
+        <button type="button">See recipe</button>
+    </li>`
+    )
+    .join('');
+}
