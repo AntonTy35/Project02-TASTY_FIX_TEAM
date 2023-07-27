a668ee225a3f72fea774fd45c297b0088ebc277e
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-07-27 09:04:20 +0200

    scroll-up

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './js/home/filters';
 import './js/home/popular-recipes';
 import './js/home/recipes';
 import './js/home/pagination';
+import './js/home/scroll-up';
 
 import './js/modal-banana-pancakes';
 import './js/modal-rating';
