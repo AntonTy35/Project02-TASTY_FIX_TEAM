eba61c1147414b56de8f4fcc63e4a2ac6270fe41
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-07-27 12:24:26 +0200

    modal add header

--- src/js/modal-order-now.js
+++ src/js/modal-order-now.js
@@ -1,10 +1,13 @@
 const refs = {
-  openModalBtn: document.querySelector('[data-modal-order-now-open]'),
+  openModalBtn: document.querySelectorAll('[data-modal-order-now-open]'),
   closeModalBtn: document.querySelector('[data-modal-order-now-close]'),
   modal: document.querySelector('[data-modal-order-now]'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
+
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
